835d21956432db919c62180b7c236a7db08fdf54
rk2810 <rohan.kumquat@gmail.com>
2017-12-20 18:54:54 +0000

    Convert JDC tree to Atomic-OS tree.

--- aos.mk
+++ aos.mk
@@ -21,11 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gemini device
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
-# Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/aos/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_gemini
+PRODUCT_NAME := aos_gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
@@ -40,6 +40,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M V8.2.1.0.NAACNEB release-keys"
 
 TARGET_VENDOR := Xiaomi
-
-TARGET_BOOT_ANIMATION_RES := 1080
-#JDC_BUILD_TYPE := ALPHA-EAS
+WITH_ROOT :=true
